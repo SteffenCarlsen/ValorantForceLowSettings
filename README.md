6ff9697acbb309f3ea2f524fb021dad551c495b2
TaF <admin@taffe.dev>
2023-09-03 15:05:27 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ValorantForceLowSettings
 Forces Valorant to low settings in the config files for all accounts on the computer
+
+## How it works
+When you set the ingame settings for Valorant, it saves these in a `GameUserSettings.ini` file for each of your accounts. This file allow you to further reduce the quality beyond "Low" ingame by setting the fields manually in the file to 0.
+
+### Doing it manually
+The program automates the following:
+
+1. Press Windows + R and type %localappdata% and press OK
+2. Access the Valorant folder
+3. Access the Saved folder
+4. Access the Config folder
+5. For each UUID in there (This is your account folders) access the Windows folder
+6. Update the `GameUserSettings.ini` file to the following:
+```
+sg.ViewDistanceQuality=0
+sg.AntiAliasingQuality=0
+sg.ShadowQuality=0
+sg.PostProcessQuality=0
+sg.TextureQuality=0
+sg.EffectsQuality=0
+sg.FoliageQuality=0
+sg.ShadingQuality=0
+```
+7. Save the file
+
+## Download
+Build it from source yourself (You need the .NET6 SDK) or download from the release section.
---
+++ ValorantSettingsOverrider/Program.cs
@@ -0,0 +1,42 @@
+﻿var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+var valorantFolder = Path.Combine(appData, "Valorant", "Saved", "Config");
+var dir = new DirectoryInfo(valorantFolder);
+if (dir.Exists) Console.WriteLine($"Config folder exists at: {valorantFolder}");
+var userFolders = dir.GetDirectories();
+var i = 0;
+foreach (var folder in userFolders)
+{
+    var userConfigFolder = new DirectoryInfo(Path.Combine(folder.FullName, "Windows"));
+    if (userConfigFolder.Exists)
+    {
+        i++;
+        var configFile = new FileInfo(Path.Combine(userConfigFolder.FullName, "GameUserSettings.ini"));
+        if (configFile.Exists)
+        {
+            var text = File.ReadLines(configFile.FullName);
+            var newText = string.Empty;
+            foreach (var line in text)
+                if (line.StartsWith("sg.") && !line.Contains("sg.ResolutionQuality"))
+                {
+                    var equalsIndex = line.IndexOf('=');
+                    if (equalsIndex != -1)
+                    {
+                        var newLine = line.Remove(equalsIndex, line.Length - equalsIndex);
+                        newLine += "=0";
+                        newText += newLine + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    newText += line + Environment.NewLine;
+                }
+
+            File.WriteAllText(configFile.FullName, newText);
+        }
+    }
+}
+
+Console.WriteLine($"Overwrote config files for {i} accounts");
+Console.WriteLine("Done!");
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
